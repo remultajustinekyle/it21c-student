63621b588901b4280423f68ad62d26be7acf2bad
Remultajustinekyle <justinekyleremulta2@gmail.com>
2023-09-29 12:06:12 +0800

    ans

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[]args) {
+    
+        Student student = new Student("Justine Kyle Remulta", "IT21C", "Study and learn more");
+        
+        System.out.println("Name: " + student.getName());
+        System.out.println("Section: " + student.getSection());
+        System.out.println("Motto: " + student.getMotto());
+       
+    }
+}
